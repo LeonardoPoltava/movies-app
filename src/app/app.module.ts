eca4befa79c6cc59cf58d7cef61e28c782655be4
LeonardoPoltava <vladyslavkrp@gmail.com>
2023-04-04 16:23:41 +0300

    Person page Deleted useless code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,8 +8,6 @@ import {AuthInterceptor} from "./http/auth.interceptor";
 import {EntrancePageModule} from "./movie/pages/entrance-page/entrance-page.module";
 import {MovieInnerPageModule} from "./movie/pages/movie-inner-page/movie-inner-page.module";
 import {UrlInterceptor} from "./http/url.interceptor";
-import { MoviesSectionComponent } from './movie/components/movies-section/movies-section.component';
-import { PeopleInnerPageComponent } from './people/pages/people-inner-page/people-inner-page.component';
 import {PeopleInnerPageModule} from "./people/pages/people-inner-page/people-inner-page.module";
 
 @NgModule({
